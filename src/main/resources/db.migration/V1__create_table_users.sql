8ef53f04d264a5a8ac340e4a8fc16b284e9f1c7a
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2025-08-07 20:34:43 -0300

    fix(auth): change column name to be according with entity

--- src/main/resources/db.migration/V1__create_table_users.sql
+++ src/main/resources/db.migration/V1__create_table_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users(
     id varchar(36) PRIMARY KEY UNIQUE NOT NULL,
-    firstName varchar not null,
-    lastName varchar,
+    first_name varchar not null,
+    last_name varchar,
     email varchar not null unique,
     password varchar not null,
     role varchar not null
