5408ad846826a634c3656d103550f6ba63e94a2f
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2025-08-08 11:28:23 -0300

    feat(auth): insert JWT decoding filter automatically in every request to get by param on controller

--- src/main/java/com/ifride/core/config/security/SecurityConfig.java
+++ src/main/java/com/ifride/core/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ifride.core.config.security;
 
+import com.ifride.core.config.security.filter.SecurityFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final SecurityFilter securityFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -34,6 +40,7 @@ public class SecurityConfig {
                         .requestMatchers("/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
