220348a634fcb11be4afd0e33f752ea02d078a90
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2025-11-25 08:22:22 -0300

    feat(driver-request): faz a classe User extender baseEntity

--- src/main/java/com/ifride/core/shared/model/BaseEntity.java
+++ src/main/java/com/ifride/core/shared/model/BaseEntity.java
@@ -1,15 +1,13 @@
 package com.ifride.core.shared.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data
 @MappedSuperclass
 public abstract class BaseEntity {
 
