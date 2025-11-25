36e2c79d8ea61497776549faf2268caf710588e5
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2025-11-25 08:15:25 -0300

    feat(driver-request): criando base entity para evitar duplicação nos campos padrão de persistência

---
+++ src/main/java/com/ifride/core/shared/model/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.ifride.core.shared.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private boolean deleted = false;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
