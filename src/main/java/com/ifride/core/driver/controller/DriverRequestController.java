c4af4416a055b0268485aaafd8fae996c00f8bb0
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2025-12-14 17:24:57 -0300

    feat(driver-request): cria método de aprovação de driver request

--- src/main/java/com/ifride/core/driver/controller/DriverRequestController.java
+++ src/main/java/com/ifride/core/driver/controller/DriverRequestController.java
@@ -3,15 +3,14 @@ package com.ifride.core.driver.controller;
 import com.ifride.core.auth.model.entity.User;
 import com.ifride.core.auth.service.UserService;
 import com.ifride.core.driver.model.dto.DriverRequestDTO;
+import com.ifride.core.driver.model.entity.Driver;
 import com.ifride.core.driver.model.entity.DriverRequest;
 import com.ifride.core.driver.service.DriverRequestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("/v1/driver-request")
 @RequiredArgsConstructor
@@ -30,4 +29,11 @@ public class DriverRequestController {
         log.info("O usuário {} iniciou a requisição de motorista para o usuário {}", author.getEmail() , user.getEmail());
         return driverRequestService.createDriverRequest(author, user, driverRequest);
     }
+
+    @PatchMapping("/{userId}/approve")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Driver approveDriverRequest(@AuthenticationPrincipal User author, @PathVariable String userId) {
+        return driverRequestService.approveDriveRequest(author, userId);
+    }
+
 }
--- src/main/java/com/ifride/core/driver/service/DriverRequestService.java
+++ src/main/java/com/ifride/core/driver/service/DriverRequestService.java
@@ -1,19 +1,21 @@
 package com.ifride.core.driver.service;
 
 import com.ifride.core.auth.model.enums.Role;
+import com.ifride.core.auth.service.UserService;
 import com.ifride.core.driver.model.dto.DriverRequestDTO;
+import com.ifride.core.driver.model.entity.Driver;
 import com.ifride.core.driver.model.entity.DriverRequest;
 import com.ifride.core.auth.model.entity.User;
 import com.ifride.core.driver.model.enums.DriverRequestStatus;
 import com.ifride.core.driver.repository.DriverRequestRepository;
 import com.ifride.core.shared.exceptions.api.ConflictException;
 import com.ifride.core.shared.exceptions.api.ForbiddenException;
-import com.ifride.core.shared.exceptions.api.NotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
+import static com.ifride.core.driver.model.enums.DriverRequestStatus.APPROVED;
 import static com.ifride.core.driver.model.enums.DriverRequestStatus.PENDING;
 
 @Service
@@ -21,6 +23,8 @@ import static com.ifride.core.driver.model.enums.DriverRequestStatus.PENDING;
 public class DriverRequestService {
 
     private final DriverRequestRepository repository;
+    private final DriverService driverService;
+    private final UserService userService;
 
     public DriverRequest createDriverRequest(User author, User user, DriverRequestDTO dto) {
         if(!userIsRequestingForHimself(author, user)) {
@@ -41,15 +45,31 @@ public class DriverRequestService {
         return repository.save(driverRequest);
     }
 
-    private DriverRequest changeDriverRequestStatus(String id, DriverRequestStatus status) {
-        var driverRequest = repository.findById(id).orElseThrow(() -> new NotFoundException("Não foi possível encontrar a requisição no sistema."));
+    public Driver approveDriveRequest(User author, String userId) {
+        var driverRequest = changeDriverRequestStatus(userId, APPROVED, author, null);
+        return driverService.saveFromDriverRequest(driverRequest);
+    }
+
+    private DriverRequest changeDriverRequestStatus(String userId, DriverRequestStatus status, User author, String rejectionReason) {
+        var driverRequest = getLastDriverRequestByUser(userId);
 
         if(driverRequest.getStatus() != PENDING) {
-            throw new ConflictException("Não é possível aprovar uma requisição que está com o status %s", driverRequest.getStatus());
+            throw new ConflictException("Não é possível modificar uma requisição que está com o status %s", driverRequest.getStatus());
         }
 
+        if(rejectionReason != null) {
+            driverRequest.setRejectionReason(rejectionReason);
+        }
+
+        driverRequest.setReviewedBy(author);
         driverRequest.setStatus(status);
         return repository.save(driverRequest);
+    }
+
+    private DriverRequest getLastDriverRequestByUser(String userId) {
+        var requester = userService.findById(userId);
+
+        return repository.findAllByRequesterOrderByCreatedAtDesc(requester).getFirst();
     }
 
     private boolean userIsRequestingForHimself(User author, User requested) {
