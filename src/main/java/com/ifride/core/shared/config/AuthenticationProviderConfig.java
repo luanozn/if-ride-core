e45c580931a3640a83296bcbc86d6b4a67a889b0
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2026-01-02 12:17:53 -0300

    feat(vehicles): Cria authenticationProvider pra permitir o Spring buscar o userDetails no banco e corrige o login

---
+++ src/main/java/com/ifride/core/shared/config/AuthenticationProviderConfig.java
@@ -0,0 +1,22 @@
+package com.ifride.core.shared.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AuthenticationProviderConfig {
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder
+    ) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+}
