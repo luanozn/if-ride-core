fbd1fb5540bf5519c8ee63910f2431c3e20e1afc
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2026-01-16 11:39:14 -0300

    feat(docs): insere swagger com validação de autenticação padrão por bearer token JWT

---
+++ src/main/java/com/ifride/core/shared/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.ifride.core.shared.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "IF Ride API",
+                version = "v1",
+                description = "Documentação da API do sistema de caronas acadêmicas",
+                contact = @Contact(name = "Luan Ribeiro", email = "luan.ribeiro@estudante.ifgoiano.edu.br")
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+}
