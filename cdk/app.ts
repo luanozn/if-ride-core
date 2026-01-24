07181db325e6c196cd0c2383a659c65830abb0e2
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2026-01-24 17:39:39 -0300

    feat(iac): cria a aplicação principal que instancia todas as outras

---
+++ cdk/app.ts
@@ -0,0 +1,37 @@
+import { App } from "aws-cdk-lib/core";
+import {VpcStack} from "./lib/vpc.stack";
+import {ConfigProps} from "./lib/utils/config-props";
+import {AssetsStack} from "./lib/assets.stack";
+import {ServerStack} from "./lib/server.stack";
+import {DatabaseStack} from "./lib/database.stack";
+
+const app = new App();
+
+const env = {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+};
+
+const configProps: ConfigProps = {
+    env,
+    parameterNames: {
+        vpcId: "vpc.id",
+        assetsBucketName: "assets.bucket.name",
+        databaseUsername: "database.username",
+        appSecurityGroupId: "network.app.security-group.id"
+    }
+}
+
+const vpc = new VpcStack(app, "VpcIfRide", configProps);
+const assets = new AssetsStack(app, "AssetsIfRide", configProps);
+const database = new DatabaseStack(app, "DatabaseStack", configProps);
+const server = new ServerStack(app, "ServerStack", configProps);
+
+server.addDependency(vpc)
+server.addDependency(assets)
+
+database.addDependency(vpc)
+database.addDependency(server)
+
+
+
