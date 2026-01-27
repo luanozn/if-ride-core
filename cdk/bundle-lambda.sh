7afd2f0057a4d530dd1bc78e83f3ecd765705b1c
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2026-01-27 20:40:40 -0300

    feat(iac): corrige cdk global e localização do arquivo no bundle-lambda

--- cdk/bundle-lambda.sh
+++ cdk/bundle-lambda.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+set -e
 
-SOURCE_DIR="../functions/authorizer"
-DEST_DIR="./dist"
+CDK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE_DIR="$CDK_DIR/../functions/authorizer"
+DEST_DIR="$CDK_DIR/dist"
 ZIP_NAME="authorizer.zip"
 
-mkdir -p $DEST_DIR
+mkdir -p "$DEST_DIR"
 
-echo "Empacotando Lambda Authorizer..."
-cd $SOURCE_DIR
-npm install --production
-zip -r ../../cdk/$DEST_DIR/$ZIP_NAME . -x "*.ts" "*.map"
+echo "Limpando dist e empacotando Lambda..."
+rm -f "$DEST_DIR/$ZIP_NAME"
 
-echo "Lambda compactado em $DEST_DIR/$ZIP_NAME"
+if [ -d "$SOURCE_DIR" ]; then
+    cd "$SOURCE_DIR"
+    npm install --omit=dev
+    zip -r "$DEST_DIR/$ZIP_NAME" . -x "*.ts" "*.map" "node_modules/.bin/*"
+    echo "Lambda compactado com sucesso em: $DEST_DIR/$ZIP_NAME"
+else
+    echo "ERRO CRÍTICO: Diretório do Authorizer não encontrado em $SOURCE_DIR"
+    exit 1
+fi
