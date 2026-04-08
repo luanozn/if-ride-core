f631e962e7b99c841692b72a1830c7e1f0da1526
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2026-04-08 08:54:51 -0300

    fix(config): insere montagem de credenciais para montagem do cliente do SES baseado no application properties

--- src/main/java/com/ifride/core/shared/config/AwsConfig.java
+++ src/main/java/com/ifride/core/shared/config/AwsConfig.java
@@ -2,6 +2,7 @@ package com.ifride.core.shared.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ses.SesClient;
 
@@ -12,6 +13,7 @@ public class AwsConfig {
     public SesClient sesClient() {
         return SesClient.builder()
                 .region(Region.SA_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
